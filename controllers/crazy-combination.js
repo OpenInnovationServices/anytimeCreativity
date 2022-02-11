09b0693a0bcb98c2103076af63dc1325e8aadc06
atish-cred <atish.pawar@credenceconsultancy.com>
2022-02-11 09:31:31 +0530

    update crazy-combination controller
    update crazy-combination controller

--- controllers/crazy-combination.js
+++ controllers/crazy-combination.js
@@ -14,10 +14,10 @@ exports.dashboard = (req, res, next) => {
     //     pageTitle: 'Crazy Combination Dashboard',
     //     data: [],
     // });
-    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function (err, db) {
+    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, db) {
         if (err) throw err;
         var dbo = db.db(keys.mongodb.dbName);
-        dbo.collection("challengeDataCollection").find({ "code": "1000" }).toArray(function (err, result) {
+        dbo.collection("challengeDataCollection").find({ "code": "1000" }).toArray(function(err, result) {
             if (err) res.status(400).json("Error Connecting DB");
             res.status(200).render('crazy-combinations/dashboard', { pageTitle: 'Crazy Combination Dashboard', data: result });
             db.close();
@@ -30,12 +30,12 @@ exports.vote = (req, res, next) => {
     //     pageTitle: 'Crazy Combination Vote',
     //     data: [],
     // });
-    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function (err, db) {
+    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, db) {
         if (err) throw err;
         var dbo = db.db(keys.mongodb.dbName);
-        dbo.collection("challengeDataCollection").find({ "code": "1000" }).toArray(function (err, result) {
+        dbo.collection("challengeDataCollection").find({ "code": "1000" }).toArray(function(err, result) {
             if (err) res.status(400).json("Error Connecting DB");
-            res.status(200).render('pages/crazyCombo-voting.ejs', { pageTitle: 'Crazy Combination Vote', data: result});
+            res.status(200).render('crazy-combinations/vote', { pageTitle: 'Crazy Combination Vote', data: result });
             db.close();
         });
     });
@@ -46,12 +46,12 @@ exports.voteDashboard = (req, res, next) => {
     //     pageTitle: 'Crazy Combination Voting Dashboard',
     //     data: [],
     // });
-    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function (err, db) {
+    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, db) {
         if (err) throw err;
         var dbo = db.db(keys.mongodb.dbName);
-        dbo.collection("challengeDataCollection").find({ "code": "1000" }).toArray(function (err, result) {
+        dbo.collection("challengeDataCollection").find({ "code": "1000" }).toArray(function(err, result) {
             if (err) res.status(400).json("Error Connecting DB");
-            res.status(200).render('pages/crazyCombo-voting-dashboard.ejs', { pageTitle: 'Crazy Combination Vote', data: result });
+            res.status(200).render('crazy-combinations/voting-dashboard', { pageTitle: 'Crazy Combination Vote', data: result });
             db.close();
         });
     });
