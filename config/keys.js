438ae19c9ca8d22f7b63a15ef7c6f924b73ea240
“LadJignesh” <jigneshlad@Jigneshs-MacBook-Air.local>
2022-02-12 18:04:12 +0000

    new DB creation

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ module.exports = {
     mongodb:{
         dbURI: "mongodb+srv://admin-fergal:2WRYDnpYMynF3BPR@cluster0.zpf6e.mongodb.net/fergalbrophySite?retryWrites=true&w=majority",
         mongoDbUri: "mongodb+srv://admin-fergal:2WRYDnpYMynF3BPR@cluster0.zpf6e.mongodb.net/fergalbrophySite?retryWrites=true&w=majority",
-        dbName:"fergalbrophySitedevdev"
+        dbName:"anytimeCreativity"
     }
 }
