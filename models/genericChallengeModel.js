c113f59e2af5d85b42a4bde28cf61542adb68c02
atish-cred <atish.pawar@credenceconsultancy.com>
2022-10-14 12:50:23 +0530

    crazy combination extra circle added.
    crazy combination extra circle added.

--- models/genericChallengeModel.js
+++ models/genericChallengeModel.js
@@ -4,9 +4,13 @@ const Schema = mongoose.Schema;
 const challengeSchema = new Schema({
     user: String,
     mix: String,
+    mix1: String,
     match: String,
+    match1: String,
     mix_text: String,
+    mix1_text: String,
     match_text: String,
+    match1_text: String,
     teamName: String,
     penName: String,
     ideaName: String,
