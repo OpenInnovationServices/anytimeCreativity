8352dbbd49f5d21648c846902ebc3236c8d00bcd
atish-cred <atish.pawar@credenceconsultancy.com>
2024-02-10 17:18:19 +0530

    UCD Medicine module added
    UCD Medicine module added

---
+++ controllers/ucd-medicine.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const keys = require('../config/keys');
+const MongoClient = require('mongodb').MongoClient;
+
+exports.index = (req, res, next) => {
+    res.render('ucd-medicine/index', {
+        pageTitle: 'Connect The Dots',
+    });
+};
+
+exports.dashboard = (req, res, next) => {
+    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(keys.mongodb.dbName);
+        dbo.collection("challengeDataCollection").find({ "code": "1002" }).toArray(function(err, result) {
+            if (err) res.status(400).json("Error Connecting DB");
+            res.status(200).render('ucd-medicine/dashboard', {
+                pageTitle: 'Connect The Dots Dashboard',
+                data: result
+            });
+            db.close();
+        });
+    });
+};
+
+exports.vote = (req, res, next) => {
+    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(keys.mongodb.dbName);
+        dbo.collection("challengeDataCollection").find({ "code": "1002" }).toArray(function(err, result) {
+            if (err) res.status(400).json("Error Connecting DB");
+            res.render('ucd-medicine/vote', {
+                pageTitle: 'Connect The Dots Vote',
+                data: result
+            });
+            db.close();
+        });
+    });
+};
+
+exports.voteDashboard = (req, res, next) => {
+    MongoClient.connect(keys.mongodb.dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(keys.mongodb.dbName);
+        dbo.collection("challengeDataCollection").find({ "code": "1002" }).toArray(function(err, result) {
+            if (err) res.status(400).json("Error Connecting DB");
+            res.render('ucd-medicine/voting-dashboard', {
+                pageTitle: 'Connect The Dots Results',
+                data: result
+            });
+            db.close();
+        });
+    });
+};
---
+++ routes/ucd-medicine.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const controller = require('../controllers/ucd-medicine');
+
+router.get('/', controller.index);
+router.get('/dashboard', controller.dashboard);
+router.get('/vote', controller.vote);
+router.get('/vote/dashboard', controller.voteDashboard);
+module.exports = router;
