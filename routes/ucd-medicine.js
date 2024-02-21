ad48fd48a39e6e55082ed5320bf9f2da8c6f0a81
atish-cred <atish.pawar@credenceconsultancy.com>
2024-02-21 18:18:12 +0530

    URL & text changes
    URL & text changes

--- routes/ucd-medicine.js
+++ routes/ucd-medicine.js
@@ -3,9 +3,9 @@ const controller = require('../controllers/ucd-medicine');
 
 router.get('/', controller.index);
 router.get('/dashboard', controller.dashboard);
-router.get('/vote1', controller.vote1);
-router.get('/vote2', controller.vote2);
+router.get('/vote', controller.vote1);
+router.get('/build', controller.vote2);
 router.get('/vote/dashboard', controller.voteDashboard);
 router.get('/idea', controller.idea);
-router.get('/result', controller.result);
+router.get('/results', controller.result);
 module.exports = router;
